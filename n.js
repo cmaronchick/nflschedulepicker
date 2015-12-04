2a9539e46f71857479d0de25dc7c9cdb74c5cf97
Eric Murphy <eric.murphy58@yahoo.com>
2015-12-04 10:17:27 -0700

    Week 13 Thursday Update

--- n.js
+++ n.js
@@ -1516,7 +1516,7 @@ function markdownExport() {
 
 function update_outcomes() {
 	
-	set_games_from_string('-WapWqmmlmamVppZmllqWqlqZVmqZVamWpaqapZaqaqmWllZVlmVmaappWmGAAAAAAAAAAAAAAAAAAAAAAAAAAA_');
+	set_games_from_string('-WapWqmmlmamVppZmllqWqlqZVmqZVamWpaqapZaqaqmWllZVlmVmaappWmWAAAAAAAAAAAAAAAAAAAAAAAAAAA_');
 	set_all_rankings();
     set_game_cookie(cookie_letters, true);
 	edit_playoffs();
